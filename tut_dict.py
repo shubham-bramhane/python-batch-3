c66587e785c7ca36df1fa86369265e0d359865c9
shubham bramhane <59164754+shubham-bramhane@users.noreply.github.com>
2022-06-14 19:15:34 +0530

    14-6

--- tut_dict.py
+++ tut_dict.py
@@ -1,92 +1,173 @@
-# explanation 
+# # explanation 
 
-# dict is a order collection of data type 
+# # dict is a order collection of data type 
 
-# dict is also represent in { }
-
-
+# # dict is also represent in { }
 
 
 
-# # example of set
-# my_set={"shubham",'ravi'}
-# print(my_set)
-# print(type(my_set))
+
+
+# # # example of set
+# # my_set={"shubham",'ravi'}
+# # print(my_set)
+# # print(type(my_set))
 
 
 
-# example of dict
+# # example of dict
+# # name={
+# #     "name":'kamesh',
+# #     "address":"ghugus",
+# #     "DoB":"19-05-1998"
+# # }
+
+# # print(name)
+# # print(type(name))
+
+# # # calculating the len
+# # print(len(name))
+
+
+# # for finding the keys
+# # print(name.keys())
+
+
+
+
+# # for finding the  value
+
+# # print(name.values())
+
+
+
+
+# # for find the items 
+
+# # print(name.items())
+
+
+
+# # print(name["name"])
+# # print(name["address"])
+# # print(name["DoB"])
+
+# # print(name.get('address'))
+
 # name={
 #     "name":'kamesh',
 #     "address":"ghugus",
 #     "DoB":"19-05-1998"
 # }
 
-# print(name)
-# print(type(name))
+# # name['gender']='male'
 
-# # calculating the len
-# print(len(name))
-
-
-# for finding the keys
-# print(name.keys())
+# # print(name)
 
 
 
+# # for adding the value 
+# # print(name)
+# # name["name"]='ravi'
+# # print(name)
 
-# for finding the  value
-
-# print(name.values())
-
-
-
-
-# for find the items 
-
-# print(name.items())
+# #  for deleting perticular value 
+# # print(name)
+# # name.pop('name')
+# # print(name)
 
 
 
-# print(name["name"])
-# print(name["address"])
-# print(name["DoB"])
-
-# print(name.get('address'))
-
-name={
-    "name":'kamesh',
-    "address":"ghugus",
-    "DoB":"19-05-1998"
-}
-
-# name['gender']='male'
-
-# print(name)
+# # #  for deleting last value 
+# # name.popitem()
+# # print(name)
 
 
 
-# for adding the value 
-# print(name)
-# name["name"]='ravi'
-# print(name)
-
-#  for deleting perticular value 
-# print(name)
-# name.pop('name')
-# print(name)
+# # create a dict having 3 key pair value 
+# # add the new element in dict 
+# # remove the element from the dict 
+# # change the value 
 
 
-
-# #  for deleting last value 
-# name.popitem()
-# print(name)
+# # for looping :
 
 
+# # students={
+# #     'name':"shubham",
+# #     'dob':"2000",
+# #     'address':"ghugus"}
 
-# create a dict having 3 key pair value 
-# add the new element in dict 
-# remove the element from the dict 
-# change the value 
+
+# # # for i in students:
+# # #     print(i)  # for key from keys
+# # #     print(students[i])    # for value from values
+
+
+
+# # # for copy()
+
+# # mydict=students.copy()
+
+# # print(mydict)
+
+
+# students={
+#     'student1':{
+#         'name':"shubham",
+#         'dob':"2000",
+#         "address":"ghugus"
+#     },
+#     "student2":{
+#         'name':"kamesh",
+#         'dob':"1998",
+#         'address':"ghugus"
+
+#     }
+# }
+
+# # for one student key
+
+# for i in students:
+#     print(students[i])
+
+
+
+# # for n of student1 or so no
+
+
+# for i in students:
+#     print(students[i]['name'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
---
+++ tut_if.py
@@ -0,0 +1,32 @@
+# if else condition 
+
+
+
+# # for  checking the Condition 
+
+# from numpy import equal, greater
+
+
+# equal a==b
+# not equal a!=b
+# les than a<b
+# greater than a>b 
+
+# less than a<=b 
+# greater tha or equal a>=b 
+
+# a=301
+# b=300
+
+# if a>=b:
+#     print('it is match')
+# else:
+#     print('it is not match')
+
+
+Num=int(input("enter the value"))
+
+if Num%2==0: 
+    print('this is even number')
+else:
+    print('this is not even number')
