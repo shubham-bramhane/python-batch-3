4cf8433320db1054bee8c9be1767bc3be1f65336
shubham bramhane <59164754+shubham-bramhane@users.noreply.github.com>
2022-06-16 19:14:12 +0530

    16-06

--- tut_while.py
+++ tut_while.py
@@ -51,5 +51,84 @@ star="*"
 #     print(star*i)
 #     i=i-1
 
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
 
 
+
+# tringle format  using while
+
+#for normal tringle
+
+# i=1
+# while i<5:
+#     num=1
+#     j=1
+#     while j<=i:
+#         print(num,end=' ')
+#         num=num+1
+#         j=j+1
+#     print( )
+#     i=i+1
+
+
+# for reverse tringle
+
+
+
+# i=5
+# while i>1:
+#     num=1
+#     j=1
+#     while j<i:
+#         print(num,end=' ')
+# #         num=num+1
+# #         j=j+1
+# #     print( )
+# #     i=i-1
+
+
+ 
+ 
+# i=1
+# num=1
+# while i<=5:
+#     j=1
+#     while j<=i:
+#         print(num,end=' ')
+#         num=num+1
+#         j=j+1
+#     print( )
+#     i=i+1
+
+
+ 
+
+# num=1
+# i=1
+# while i<10:
+#     print(num,end=' ')
+#     num=num+1
+
+
+
+a=[10,20,30,40,50,60,70]
+
+# a 
+# a b
+# a b c 
+# a b c d 
+
+i=0
+while i<len(a):
+    j=0
+    while j<=i:
+        print(a[j],end=' ')
+        j=j+1
+    print()
+    i=i+1
+
+
+# create a program for reverse tringle 
